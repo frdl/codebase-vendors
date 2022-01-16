6dd7480cafd7ba713c9bd47beb68a6dbbb0732f0
Till Wehowski <github@webfan.de>
2022-01-16 22:58:53 +0100

    Update license.php

--- public/license.php
+++ public/license.php
@@ -1,4 +1,5 @@
 <?php
 header('Content-Type: text/plain');
+header('Access-Control-Allow-Origin: text/plain');
 
 echo file_get_contents(__DIR__.\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'licenses.md');
