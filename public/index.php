43c2a2e0e5a753429785452d43428302a987415f
Till Wehowski <github@webfan.de>
2022-09-21 18:38:34 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -41,6 +41,48 @@ require __DIR__.'/../vendor/autoload.php';
 
 
 
+$pub_key_file = __DIR__ . ''.  \DIRECTORY_SEPARATOR. '..' .  \DIRECTORY_SEPARATOR. '.api-keys'.  \DIRECTORY_SEPARATOR. 'pub.key';
+$priv_key_file = __DIR__ . ''.  \DIRECTORY_SEPARATOR. '..'.  \DIRECTORY_SEPARATOR. '.api-keys'.  \DIRECTORY_SEPARATOR. 'priv.key';
+
+if(!is_dir(dirname($pub_key_file))){
+  mkdir(dirname($pub_key_file), 0755, true);	
+}
+
+if(!is_dir(dirname($priv_key_file))){
+  mkdir(dirname($priv_key_file), 0755, true);	
+}
+
+if(!file_exists($pub_key_file) || !file_exists($priv_key_file) ){
+   $config = array(
+    "digest_alg" => "sha512",
+    "private_key_bits" => 4096,
+    "private_key_type" => \OPENSSL_KEYTYPE_RSA,
+   );
+   
+   // Create the private and public key
+   $res = openssl_pkey_new($config);
+
+  // Extract the private key from $res to $privKey
+   openssl_pkey_export($res, $privKey);
+
+    // Extract the public key from $res to $pubKey
+    $pubKey = openssl_pkey_get_details($res);
+    $pubKey = $pubKey["key"];
+
+	
+	file_put_contents($pub_key_file, $pubKey);
+	file_put_contents($priv_key_file, $privKey);
+     //$data = 'plaintext data goes here';
+
+// Encrypt the data to $encrypted using the public key
+     //openssl_public_encrypt($data, $encrypted, $pubKey);
+
+// Decrypt the data using the private key and store the results in $decrypted
+     //openssl_private_decrypt($encrypted, $decrypted, $privKey);
+}
+
+
+
 $per_page = 25;
 
 $someDirs=[	
@@ -179,6 +221,10 @@ if(empty($content) && isset($_GET['source']) && '*'!==$_GET['source']){
 			$code = (new \Nette\PhpGenerator\PsrPrinter)->printFile($FileAll);
 		 
 						
+			$code = false === strpos($code, base64_decode('X19oYWx0X2NvbXBpbGVyKCk7')) 
+					  ? $loader->sign($code, file_get_contents($priv_key_file), 'X19oYWx0X2NvbXBpbGVyKCk7')
+					  : $code;	
+						
 			if((isset($_SERVER['HTTP_X_SOURCE_ENCODING']) && 'b64' === $_SERVER['HTTP_X_SOURCE_ENCODING'])
 			    || 
 			   (isset($_GET['source-encoding']) && 'b64' === $_GET['source-encoding'] )
@@ -267,6 +313,13 @@ if(empty($content) && isset($_GET['source']) && '*'!==$_GET['source']){
 			}
 
 					
+							
+			$outPut = false === strpos($outPut, base64_decode('X19oYWx0X2NvbXBpbGVyKCk7')) 
+					  ? $loader->sign($outPut, file_get_contents($priv_key_file), 'X19oYWx0X2NvbXBpbGVyKCk7')
+					  : $outPut;	
+			
+			
+			
 			if((isset($_SERVER['HTTP_X_SOURCE_ENCODING']) && 'b64' === $_SERVER['HTTP_X_SOURCE_ENCODING'])
 			    || 
 			   (isset($_GET['source-encoding']) && 'b64' === $_GET['source-encoding'] )
