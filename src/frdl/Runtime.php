66d6fdf3f1b209b549a165874a755b69c3cf25b9
Till Wehowski <github@webfan.de>
2022-10-27 16:43:08 +0200

    Update Runtime.php

--- src/frdl/Runtime.php
+++ src/frdl/Runtime.php
@@ -19,7 +19,50 @@ namespace frdl{
 		  });
 		 
 	
-		 $this->obj->prototype = \frdl\create([    
+		
+	$this->obj->prototype = \frdl\create([  			 
+	 'getFormFromRequestHelper' =>	function(string $message = '',
+											 bool $autosubmit = true, 
+											 $delay = 0,
+											 \Psr\Http\Message\ServerRequestInterface $request = null){
+	 if(null === $request){
+		 $request = (null === $this->getContainer(false) || !$this->getContainer(false)->has('request'))
+			 ? null : $this->container->get('request');
+	 }
+	 $vars = (null===$request)
+		 ? $_POST 
+		 : $request->getParsedBody();
+	
+	 $target =  (null===$request)
+		 ? $_SERVER['REQUEST_URI']
+		 : $request->getParsedBody();	 
+		 
+	 $method =  (null===$request)
+		 ? $_SERVER['REQUEST_METHOD']
+		 : $request->getMethod();	 
+		 
+	 $vars = (array)$vars;
+	
+	 $id = 'idr'.str_pad(time(), 32, "0", \STR_PAD_LEFT).str_pad(mt_rand(1,99999999), 8, "0", \STR_PAD_LEFT); 
+	
+	 $html = $message;
+	 $html.='<form id="'.$id.'" action="'.$target.'" method="'.$method.'">';
+	 foreach($vars as $n => $v){
+		$html.='<input type="hidden" name="'.$n.'" value="'.strip_tags($v).'" />';
+	 }
+	 $html.='</form>';	
+	
+	 if(true === $autosubmit){
+		$html.='<script>';
+		$html.='(()=>{';
+		 $html.='setTimeout(()=>{document.getElementById(\''.$id.'\').submit();}, '.$delay.')';
+		$html.='})();';
+		$html.='</script>';
+	 }
+	
+	  return $html;
+    },	 
+			 
 			 'addContainer' => function (\Psr\Container\ContainerInterface $container) {     
 				 return $this->getContainer()->addContainer($container); 
 			 },			 
@@ -32,6 +75,9 @@ namespace frdl{
 					 $this->setKernel( \Webfan\Webfat\App\Kernel::mutex() ); 
 				 }
 				 return $this->kernel;
+			 },	 
+			 'isLocalhost' => function () :bool {   
+				 return $_SERVER['REMOTE_ADDR'] === '127.0.0.1';
 			 },
 		 ]);		 
 		
