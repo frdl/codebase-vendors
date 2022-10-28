1f67e22f33d4eea8986a72aba8a51cb2290b4972
Till Wehowski <github@webfan.de>
2022-10-28 04:13:14 +0200

    Update AdvancedWebAppInterface.php

--- src/Frdlweb/AdvancedWebAppInterface.php
+++ src/Frdlweb/AdvancedWebAppInterface.php
@@ -12,8 +12,6 @@ interface AdvancedWebAppInterface extends WebAppInterface
     public function setStub(?StubHelperInterface $stubHelper = null) : AppInterface;
     public function getSources(string $userdir = null, $create = false) : array;
     public function getWebUriBase() : string | bool;
-    public static function getInstance(string $env = null, string $dir = null): AppInterface;
-    public static function mutex(): AppInterface;
     public function getAppId() : string;
     public function mount(): AppInterface;
     public function setAppId(string $appId = '1.3.6.1.4.1.37553.8.1.8.8.1958965301') : ?AppInterface;
