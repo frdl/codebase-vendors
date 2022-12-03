9e7437af8b47e88af987c604db9154cbd7404375
Till Wehowski <github@webfan.de>
2022-12-03 11:30:28 +0100

    Create KernelHelperInterface.php

---
+++ src/Frdlweb/KernelHelperInterface.php
@@ -0,0 +1,29 @@
+<?php
+namespace Frdlweb;
+
+
+
+interface KernelHelperInterface 
+{
+  
+	const PROTECTED_SCHEMES = [
+		'tcp',
+		'file',
+		'http',
+		'https',
+		'ws',
+		'php',
+		'tmp',
+		'phar',
+		'zip',
+        'ftp',
+        'sftp',
+        'compress.bzip2',
+        'compress.zlib',
+
+	];
+  
+  
+	
+  
+}
