07ecb2ab0fe2a8268b7ec91bf6d08de5f3c06514
Till Wehowski <github@webfan.de>
2023-02-01 05:16:59 +0100

    Update GeneratorInterface.php

--- src/Frdlweb/Contract/Autoload/GeneratorInterface.php
+++ src/Frdlweb/Contract/Autoload/GeneratorInterface.php
@@ -5,11 +5,9 @@ namespace Frdlweb\Contract\Autoload;
 	
 	interface GeneratorInterface {	
 	   public function withContext(Context $Context); 		
-	   public function withPackage(string | array | \stdclass $urlPackageNameOrComposerJson ); 			
+	   public function withPackage(string | array | object $urlPackageNameOrComposerJson ); 			
 	   public function withDirectory($dir); 
 	   public function withAlias(string $alias, string $rewrite); 
 	   public function withClassmap(array $classMap = null); 
 	   public function withNamespace($prefix, $server, $prepend = false);
 	}
-	
-    
