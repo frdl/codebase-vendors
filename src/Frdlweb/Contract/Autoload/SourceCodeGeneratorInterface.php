0a8aaf0bb0633dcfb72e9135ee913dd2e5901741
Till Wehowski <github@webfan.de>
2023-02-01 04:26:42 +0100

    Update SourceCodeGeneratorInterface.php

--- src/Frdlweb/Contract/Autoload/SourceCodeGeneratorInterface.php
+++ src/Frdlweb/Contract/Autoload/SourceCodeGeneratorInterface.php
@@ -1,8 +1,11 @@
 <?php
-
-interface SourceCodeGeneratorInterface
-{
+namespace Frdlweb\Contract\Autoload {
+ if (!interface_exists(SourceCodeGeneratorInterface::class)) {	     
+  interface SourceCodeGeneratorInterface
+   {
      public function file(string $className):string;
      public function source(string $className):string;
      public function bundle(array $classes):string;   
+   }
+  }     
 }
