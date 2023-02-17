d151d2dbd678f67dc62236f280b135375548421c
Till Wehowski <github@webfan.de>
2023-02-17 03:34:48 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,32 @@
 <?php
+
+
 namespace frdl{
 
 function create($arg)
 {
+    trigger_error('frdl\create is deorecated!', \E_USER_DEPRECATED);
     return Proto::create($arg);
 }
   
-}
+}//ns frdl;
+
+namespace Webfan\Codebase\Server {
+    
+ function init(){
+     
+     //patches
+     \frdl\implementation\psr4\RemoteAutoloaderApiClient::getInstance('https://webfan.de/install/stable/?source={{class}}&salt={{salt}}', true)       
+         ->withClassmap([
+           \Webfan\Codebase\Server\BundleExportHelper::class => 'https://webfan.de/install/?source=Webfan\Codebase\Server\BundleExportHelper&salt=${salt}',
+           \frdl\Proto::class => 'https://webfan.de/install/?source=frdl\Proto&salt=${salt}',
+       ]); 
+     
+     
+ }   
+    
+    
+    
+    
+}//ns Webfan\Codebase\Server;
+
