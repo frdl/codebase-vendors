5b0c079729832cf222bd6bf2289c3ac3fc8d4d61
Till Wehowski <github@webfan.de>
2023-02-17 13:57:11 +0100

    Update WebAppInterface.php

--- src/Frdlweb/WebAppInterface.php
+++ src/Frdlweb/WebAppInterface.php
@@ -1,9 +1,13 @@
 <?php
 namespace Frdlweb;
 
-use Frdlweb\AppInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Container\ContainerInterface;
 
-interface WebAppInterface extends AppInterface
+
+interface WebAppInterface extends RequestHandlerInterface
 {
-  
+	public function getContainer() : ContainerInterface;	
+	public function handleCliRequest();	
+	public function handleHttpRequest(\Psr\Http\Message\ServerRequestInterface $request = null) :?ResponseInterface;	
 }
